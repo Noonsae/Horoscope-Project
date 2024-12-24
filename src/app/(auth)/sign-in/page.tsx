73b4c2f08f6f894d0afbdbe88b902f3e669ea961
wooseok <youkn0wthat@naver.com>
2024-12-25 02:43:34 +0900

    feat: 로그인 페이지 구현

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,9 +1,47 @@
-import React from 'react'
+'use client';
 
-const page = () => {
+import { useSignInMutation } from '@/hooks/useSignInMutation';
+import Link from 'next/link';
+import { useState } from 'react';
+
+const Page: React.FC = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { mutate } = useSignInMutation();
+
+  const handleLogin = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    mutate({ email, password });
+  };
   return (
-    <div>page</div>
-  )
-}
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <form onSubmit={handleLogin} className="bg-white p-6 rounded-lg shadow-md w-80">
+        <label className="block text-gray-700 text-sm font-medium mb-2">이메일</label>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="block w-full px-4 py-2 border rounded-lg text-sm text-gray-900 border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+        />
+        <label className="block text-gray-700 text-sm font-medium mt-4 mb-2">비밀번호</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="block w-full px-4 py-2 border rounded-lg text-sm text-gray-900 border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+        />
+        <button
+          type="submit"
+          className="w-full mt-6 bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 focus:ring-4 focus:ring-blue-300 transition"
+        >
+          로그인
+        </button>
+      </form>
+      <Link href="/sign-up">
+        <p>계정이 없으신가요? 회원가입하러 가기</p>
+      </Link>
+    </div>
+  );
+};
 
-export default page
+export default Page;
