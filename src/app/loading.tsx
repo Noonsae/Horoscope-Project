a73c4c448a73a92c5ec39ee6e6af1938fc4f08cb
wooseok <youkn0wthat@naver.com>
2024-12-24 20:06:30 +0900

    feat: 회원가입 구현현

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,1 +1,2 @@
 // loading
+'use client';
--- src/supabase/supabase.ts
+++ src/supabase/supabase.ts
@@ -1,20 +1,26 @@
 // supabase
 
-import { createClient } from '@supabase/supabase-js';
+// import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseKey = process.env.NEXT_PUBLIC_VITE_SUPABASE_KEY!;
-const supabase = createClient(supabaseUrl, supabaseKey);
+// const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+// const supabaseKey = process.env.NEXT_PUBLIC_VITE_SUPABASE_KEY!;
+// const supabase = createClient(supabaseUrl, supabaseKey);
 
-export default supabase;
+// export default supabase;
 
 // ssr 사용할 경우 아래 로직 사용 예정입니다.
 
-// import { createBrowserClient } from "@supabase/ssr";
-// export const createClient = () =>
-//   createBrowserClient(
-//     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-//     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-//   );
-// const browserClient = createClient();
-// export default browserClient;
+import { createBrowserClient } from '@supabase/ssr';
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+// 환경 변수 값이 비어 있으면 명확한 오류 발생
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error('Supabase URL 또는 anon key가 누락되었습니다. .env 파일을 확인하세요.');
+}
+
+export const createClient = () => createBrowserClient(supabaseUrl, supabaseAnonKey);
+
+const browserClient = createClient();
+export default browserClient;
