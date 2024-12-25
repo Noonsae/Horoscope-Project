d77f7a34c65738d1a19d76eb5ffb37e8c80c3201
wooseok <youkn0wthat@naver.com>
2024-12-25 15:06:04 +0900

    feat: 로그인 css

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -14,33 +14,39 @@ const Page: React.FC = () => {
     mutate({ email, password });
   };
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <form onSubmit={handleLogin} className="bg-white p-6 rounded-lg shadow-md w-80">
-        <label className="block text-gray-700 text-sm font-medium mb-2">이메일</label>
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          className="block w-full px-4 py-2 border rounded-lg text-sm text-gray-900 border-gray-300 focus:ring-blue-500 focus:border-blue-500"
-        />
-        <label className="block text-gray-700 text-sm font-medium mt-4 mb-2">비밀번호</label>
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          className="block w-full px-4 py-2 border rounded-lg text-sm text-gray-900 border-gray-300 focus:ring-blue-500 focus:border-blue-500"
-        />
-        <button
-          type="submit"
-          className="w-full mt-6 bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 focus:ring-4 focus:ring-blue-300 transition"
-        >
-          로그인
-        </button>
-      </form>
-      <Link href="/sign-up">
-        <p>계정이 없으신가요? 회원가입하러 가기</p>
-      </Link>
-    </div>
+    <>
+      <div className="flex flex-col items-center justify-center min-h-screen w-full h-64 bg-gradient">
+        <form onSubmit={handleLogin} className="bg-[#262626] p-6 rounded-lg shadow-md w-80">
+          <label className="block text-white text-sm font-medium mb-2">이메일</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="아이디를 입력해주세요."
+            className="block w-full px-4 py-2 border rounded-lg text-sm text-gray-900 border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+          />
+          <label className="block text-white text-sm font-medium mt-4 mb-2">비밀번호</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="비밀번호를 입력해주세요."
+            className="block w-full px-4 py-2 border rounded-lg text-sm text-gray-900 border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+          />
+          <button
+            type="submit"
+            className="w-full mt-6 bg-red-600 text-white py-2 rounded-lg focus:ring-4 transition font-bold"
+          >
+            로그인
+          </button>
+        </form>
+        <Link href="/sign-up">
+          <p className="text-sm text-center text-[#AAAAAA] mt-4 hover:underline font-bold">
+            계정이 없으신가요? <span className="text-red-600"> 회원가입하러 가기 </span>
+          </p>
+        </Link>
+      </div>
+    </>
   );
 };
 
