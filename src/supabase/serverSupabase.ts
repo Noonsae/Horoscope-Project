5a13af55c29510d6f23e17aa5b40269aade1aaf7
wooseok <youkn0wthat@naver.com>
2024-12-26 10:09:38 +0900

    feat: 서버클라이언트 작성중 (현재 오류 발생 수정중)

--- src/supabase/serverSupabase.ts
+++ src/supabase/serverSupabase.ts
@@ -1,15 +1,12 @@
 import { createServerClient } from '@supabase/ssr';
 
 export const supabaseServerClient = (cookies: Record<string, string | undefined> = {}) => {
-  const supabaseUrl = process.env.SUPABASE_URL;
-  const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
   if (!supabaseUrl || !supabaseAnonKey) {
-    throw new Error('환경 변수 SUPABASE_URL 또는 SUPABASE_ANON_KEY가 누락되었습니다.');
+    throw new Error('SUPABASE_URL 또는 SUPABASE_ANON_KEY 환경 변수가 누락되었습니다.');
   }
-
-  // 기본값 설정 (필요한 경우)
-  cookies = cookies || {};
 
   return createServerClient(supabaseUrl, supabaseAnonKey, { cookies });
 };
