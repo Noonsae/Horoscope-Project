344a64516b892e949509c17bd3758cb8588a8496
Noon_sae <minseock7308@gmail.com>
2024-12-26 10:04:26 +0900

    feat : my-page contents 생성

--- src/app/my-page/_components/Contents.tsx
+++ src/app/my-page/_components/Contents.tsx
@@ -1,15 +1,64 @@
 import React from 'react';
-import { FortuneList, CommentBtnList } from '.';
+import { FortuneList, CommentList } from './'; // 각 콘텐츠 컴포넌트 임포트
+import { Comment } from '@/types/guestbook-type'; // Comment 타입 임포트
 
 interface ContentsProps {
-  activeTab: 'fortune' | 'comments';
+  activeTab: 'fortune' | 'comments' | 'profile';
+  comments: Comment[];
+  newProfileImg: string | File | null;
+  confirmDeleteComment: (id: string) => void;
+  newNickname: string;
+  setNewNickname: (value: string) => void;
+  handleSubmit: (e: React.FormEvent<HTMLFormElement>) => Promise<void>; // 수정된 부분
+  commentsPending: boolean;
+  commentsError: boolean;
 }
 
-const Contents: React.FC<ContentsProps> = ({ activeTab }) => {
+const Contents: React.FC<ContentsProps> = ({
+  activeTab,
+  comments,
+  newProfileImg,
+  confirmDeleteComment,
+  newNickname,
+  setNewNickname,
+  handleSubmit,
+  commentsPending,
+  commentsError
+}) => {
   return (
-    <div className="w-full h-[800px] flex items-center justify-center bg-gradient text-white">
+    <div className="w-full h-[800px]">
       {activeTab === 'fortune' && <FortuneList />}
-      {activeTab === 'comments' && <CommentBtnList />}
+      {activeTab === 'comments' && (
+        <>
+          {commentsPending ? (
+            <p>로딩중입니다...</p>
+          ) : commentsError ? (
+            <p className="font-semibold">댓글 데이터를 가져오는 중 에러가 발생했습니다.</p>
+          ) : (
+            <CommentList
+              comments={comments}
+              newProfileImg={newProfileImg}
+              confirmDeleteComment={confirmDeleteComment}
+            />
+          )}
+        </>
+      )}
+      {activeTab === 'profile' && (
+        <div>
+          {activeTab === 'profile' && (
+            <form onSubmit={handleSubmit}>
+              <p>프로필 변경</p>
+              <input
+                type="text"
+                value={newNickname}
+                onChange={(e) => setNewNickname(e.target.value)}
+                placeholder="변경하실 닉네임을 입력해주세요."
+              />
+              <button type="submit">프로필 저장</button>
+            </form>
+          )}
+        </div>
+      )}
     </div>
   );
 };
