db26ee4df1aac3140e9556ce21354ed85dcfe211
wooseok <youkn0wthat@naver.com>
2024-12-27 10:21:53 +0900

    refactor: 임포트 경로 수정

--- src/hooks/useStellaQuery.ts
+++ src/hooks/useStellaQuery.ts
@@ -1,13 +1,9 @@
 import { useQuery } from '@tanstack/react-query';
-import browserClient from '@/lib/supabase-client';
 import { stellas } from '@/types/stellas-type';
+import { supabase } from '@/lib/supabase';
 
 const fetchStellaData = async (id: string): Promise<stellas> => {
-  const { data, error } = await browserClient
-    .from('stellas')
-    .select('id, name, description, img_url')
-    .eq('id', id)
-    .single();
+  const { data, error } = await supabase.from('stellas').select('id, name, description, img_url').eq('id', id).single();
 
   if (error) {
     console.error('별자리 데이터 불러오기 실패!', error);
