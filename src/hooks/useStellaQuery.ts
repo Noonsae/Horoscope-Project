8486871c636e7bacb0b864f0aae6be689f866851
tkddyd0817 <tkddyd1259@gmail.com>
2024-12-27 10:15:10 +0900

    feat: 별자리정보 리팩토링 완료 신년운세 기능구현 리펙토링할예정정

--- src/hooks/useStellaQuery.ts
+++ src/hooks/useStellaQuery.ts
@@ -1,13 +1,9 @@
 import { useQuery } from '@tanstack/react-query';
-import browserClient from '@/lib/supabase-client';
+import { supabase } from '@/lib/supabase';
 import { stellas } from '@/types/stellas-type';
 
-const fetchStellaData = async (id: string): Promise<stellas> => {
-  const { data, error } = await browserClient
-    .from('stellas')
-    .select('id, name, description, img_url')
-    .eq('id', id)
-    .single();
+export const fetchStellaData = async (id: string): Promise<stellas> => {
+  const { data, error } = await supabase.from('stellas').select('id, name, description, img_url').eq('id', id).single();
 
   if (error) {
     console.error('별자리 데이터 불러오기 실패!', error);
