d7a3da09cadf0fed8295d184ee9b6fcc4fce8771
wooseok <youkn0wthat@naver.com>
2024-12-27 10:20:11 +0900

    refactor: 임포트 경로 수정

--- src/components/home/ClientHomePage.tsx
+++ src/components/home/ClientHomePage.tsx
@@ -1,24 +1,22 @@
 'use client';
 
-import { useEffect, useState } from "react";
-import UserHomePage from "./UserHomePage";
-import GuestHomePage from "./GuestHomePage";
-import clientSupabase from "@/lib/supabase-client";
-
-const supabase = browserClient;
+import { useEffect, useState } from 'react';
+import UserHomePage from './UserHomePage';
+import GuestHomePage from './GuestHomePage';
+import { supabase } from '@/lib/supabase';
 
 const ClientHomePage = () => {
   const [isLogin, setIsLogin] = useState(false);
 
   useEffect(() => {
     const ckeckLoginStatus = async () => {
-      const { data } = await clientSupabase.auth.getSession();
+      const { data } = await supabase.auth.getSession();
       setIsLogin(!!data.session);
     };
 
     ckeckLoginStatus();
 
-    const { data: listener } = clientSupabase.auth.onAuthStateChange((_envent, session) => {
+    const { data: listener } = supabase.auth.onAuthStateChange((_envent, session) => {
       setIsLogin(!!session);
     });
 
