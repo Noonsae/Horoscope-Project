63583d5f8ad9befdeaf09b44c062431af7323b96
wooseok <youkn0wthat@naver.com>
2024-12-27 19:33:27 +0900

    feat: supabase 로직 수정

--- src/lib/supabase-client.ts
+++ src/lib/supabase-client.ts
@@ -1,4 +1,11 @@
+// @lib/supabase-client.ts
+
+import { Database } from '@/types/supabase-type';
 import { createBrowserClient } from '@supabase/ssr';
 
-export const browserSupabase = () =>
-  createBrowserClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!);
+export const browserSupabase = () => {
+  return createBrowserClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
+};
