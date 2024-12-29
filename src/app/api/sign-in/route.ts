bdf4a5446d7aff61ed31475ac4683aff62fc0d6a
Noon_sae <minseock7308@gmail.com>
2024-12-29 09:54:39 +0900

    feat : 디버깅 작업 중..

--- src/app/api/sign-in/route.ts
+++ src/app/api/sign-in/route.ts
@@ -1,5 +1,3 @@
-"use server"
-
 import { serverSupabase } from '@/lib/supabase-server';
 
 import { NextResponse } from 'next/server';
@@ -7,13 +5,19 @@ import { NextResponse } from 'next/server';
 export async function POST(request: Request) {
 
   const supabase = serverSupabase();
+  console.log('Supabase 객체:', supabase);
 
   const { email, password } = await request.json();
   console.log('요청 데이터:', { email, password });
+
+  if (!supabase || typeof supabase.auth === 'undefined') {
+    console.error('Supabase 클라이언트가 제대로 초기화되지 않았습니다.');
+    return NextResponse.json({ errorMsg: 'Supabase 클라이언트 초기화 실패' }, { status: 500 });
+  }
   
   try {
     const { data, error } = await supabase.auth.signInWithPassword({ email, password });
-    
+
     if (error) {
       console.error('Supabase 인증 오류:', error.message);
       return NextResponse.json({ errorMsg: error.message }, { status: 401 });
