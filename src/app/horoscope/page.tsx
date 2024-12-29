f956b6e0829660d19fb2eef5d05bc81c45558351
Noon_sae <minseock7308@gmail.com>
2024-12-30 06:03:51 +0900

    feat : horoscope css

--- src/app/horoscope/page.tsx
+++ src/app/horoscope/page.tsx
@@ -19,9 +19,9 @@ const HoroscopePage: React.FC = () => {
   };
 
   return (
-    <div className="flex items-center justify-center h-screen py-2 px-4">
-      <div className="flex flex-col text-center border border-gray-300 rounded-lg shadow-lg bg-white p-6 w-[700px] h-[600px]">
-        <h1 className="mb-4 text-2xl font-bold">당신의 운세를 확인해보세요</h1>
+    <div className="flex items-center justify-center min-h-screen bg-gradient">
+      <div className="flex flex-col text-center rounded-[20px] -mt-[100px] shadow-lg bg-white w-[700px] h-[600px] py-[30px]">
+        <h1 className="text-[34px] font-bold">당신의 운세를 확인해보세요</h1>
         <form onSubmit={handleSubmit} className="flex flex-col items-center justify-between flex-grow">
           <div className="bg-white-500 rounded-lg p-4 w-[600px] h-[500px] flex flex-col items-center justify-between">
             <div className="flex items-center justify-center flex-grow">
