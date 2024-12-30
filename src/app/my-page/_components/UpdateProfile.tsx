11ecaf9badfd4321f6bf80c3fb7aba3ba35cb7d3
Noon_sae <minseock7308@gmail.com>
2024-12-30 10:22:48 +0900

    feat : 버그를 찾긴했는데 .. ㅠㅠ 배포먼저하고.. 해보시져

--- src/app/my-page/_components/UpdateProfile.tsx
+++ src/app/my-page/_components/UpdateProfile.tsx
@@ -8,24 +8,23 @@ interface UpdateProfileProps {
 
 const UpdateProfile: React.FC<UpdateProfileProps> = ({ newNickname, setNewNickname, handleSubmit }) => {
   return (
-    <form onSubmit={handleSubmit} className="rounded-xl flex flex-col items-center">
-      <div className="w-[500px] flex flex-col items-start mt-4">
-        <label className="text-[26px] mb-2 text-white">닉네임 변경</label>
-        <input
-          type="text"
-          value={newNickname}
-          placeholder="변경하실 닉네임을 입력해주세요."
-          className="mt-1 block text-black w-full bg-white border border-gray-700 rounded-xl p-2 border-none pl-4"
-          onChange={(e) => setNewNickname(e.target.value)}
-        />
-      </div>
-      <button
-        type="submit"
-        className="w-[120px] mt-10 bg-[#EC4C4C] text-lg py-[6px] rounded-3xl hover:bg-[#B73838] font-semibold"
-      >
-        변경 하기
-      </button>
-    </form>
+    <div className="max-w-[600px] mx-auto mt-10">
+      <form onSubmit={handleSubmit} className="flex flex-col items-center">
+        <div className="mb-6 w-[400px]">
+          <label className="block font-bold mb-2 text-white text-[26px]">닉네임 변경</label>
+          <input
+            type="text"
+            value={newNickname}
+            placeholder="변경하실 닉네임을 입력해주세요."
+            className="mt-1 block text-black w-full bg-white border border-gray-700 rounded-[10px] p-2 pl-4"
+            onChange={(e) => setNewNickname(e.target.value)}
+          />
+        </div>
+        <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded-[10px] w-[400px]">
+          저장하기
+        </button>
+      </form>
+    </div>
   );
 };
 
